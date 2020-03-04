a3bd04eb71054f75550dc0d1a8db1335a34adff7
Taylor Slye <slyet@oregonstate.edu>
2020-03-04 11:47:50 -0800

    add return function to rental

--- customer_returns_rentable_late_workflow.rb
+++ customer_returns_rentable_late_workflow.rb
@@ -1,12 +1,12 @@
 class CustomerReturnsRentableLateWorkflow
   
-  def initialize(rental, emloyee)
+  def initialize(rental, emloyee, return_time)
     @rental = rental
     @employee = employee
   end
 
   def run
-    @rental.return
+    @rental.return(return_time)
     @rental.charge
     @employee.report_issues(@rental)
   end
--- rental.rb
+++ rental.rb
@@ -1,9 +1,15 @@
 class Rental
-  def initialize(start_date, end_date, rental_object)
+  def initialize(start_date, end_date, rental_object, customer)
     @start_date = start_date
     @end_date = end_date
     @rental_object = rental_object
+    @customer = customer
   end
 
+  def return(time)
+    @return_time = time 
+  end
 
+  def charge
+  end
 end
