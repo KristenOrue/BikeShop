57eb943c7f45c19c90d57e7b0f51a31d141eee17
Taylor Slye <slyet@oregonstate.edu>
2020-03-08 21:35:45 -0700

    add a charge, latesurchage, and late notice function to rental class

--- rental.rb
+++ rental.rb
@@ -1,4 +1,6 @@
 class Rental
+  RENTAL_FEE = 2
+  
   def initialize(start_date, end_date, rental_object, customer)
     @start_date = start_date
     @end_date = end_date
@@ -7,9 +9,26 @@ class Rental
   end
 
   def return(time)
-    @return_time = time 
+    @return_time = time
   end
 
   def charge
+    @totalprice = (return_time - start_date) * rental_object.rate
+    self.latesurcharge
+    customer.funds -= @totalprice
   end
+
+  def latesurcharge
+    if return_date + 30 > end_date
+      @totalprice += RENTAL_FEE
+      if @latenotice == true
+	@totalprice -= .5 * RENTAL_FEE
+      end
+    end
+  end
+
+  def latenotification
+    @latenotice = 2
+  end
+
 end
